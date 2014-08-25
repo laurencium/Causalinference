88fb3465d5d85bad0a6eef7a1a294a11bd0ca65e
Laurence Wong <laurencium@gmail.com>
2014-08-24 23:10:03 -0700

    synthetic control can now do ATE, ATUT

--- causal.py
+++ causal.py
@@ -18,24 +18,29 @@ class CausalModel(object):
 		self.N_c = self.N - self.N_t
 
 
-	def __polymatrix(self, X, terms):
+	def __polymatrix(self, X, poly):
 
 		"""
-		Constructs matrix of polynomial terms to be used in synthetic control matching.
+		Constructs matrix of polynomial terms to be used in synthetic control
+		matching.
 
 		Arguments
 		---------
 			X: array-like
-				k Covariate values/vectors from which higher order polynomial terms are
-				to be constructed.
-			terms: list
-				List of specific combinations of covariates to multiply together.
+				k Covariate values/vectors from which higher order polynomial
+				terms are to be constructed.
+			poly: Integer
+				Degree of polynomial to generate.
 
 		Returns
 		-------
 			Original covariates matrix appended with higher polynomial terms.
 		"""
 
+		terms = []
+		for power in xrange(2, poly+1):
+			terms.extend(list(itertools.combinations_with_replacement(range(self.k),
+			                                                          power)))
 		num_of_terms = len(terms)
 		X_poly = np.ones((X.shape[0], num_of_terms))
 		for i in xrange(num_of_terms):
@@ -44,128 +49,69 @@ class CausalModel(object):
 		return np.column_stack((X, X_poly))
 
 
+	def __synthetic(self, X):
+
+		'''
+		Compute individual-level treatment effect by applying synthetic
+		control method on input covariate matrix X. The public function 
+		self.synthetic is a wrapper around this.
+
+		Arguments
+		---------
+			X: array-like
+				Covariate matrix used to compute synthetic controls. Does
+				not necessarily have to be self.X.
+
+		Returns
+		-------
+			Vector of individual-level treatment effects.
+
+		'''
+
+		ITT = np.empty(self.N)
+
+		for i in xrange(self.N):
+			if self.D[i]:
+				w = np.linalg.lstsq(np.row_stack((X[self.controls].T, np.ones(self.N_c))), np.append(X[i],1))[0]
+				ITT[i] = self.Y[i] - np.dot(w, self.Y[self.controls])
+			else:
+				w = np.linalg.lstsq(np.row_stack((X[self.treated].T, np.ones(self.N_t))), np.append(X[i],1))[0]
+				ITT[i] = np.dot(w, self.Y[self.treated]) - self.Y[i]
+
+		return ITT
+
+
 	def synthetic(self, poly=0):
 
 		"""
-		Estimates the average treatment effect for the treated (ATT) via synthetic
-		controls.
+		Estimate the average treatment effects via synthetic controls.
 
-		Terms of higher order polynomials of the covariates can be used in constructing
-		synthetic controls.
+		Terms of higher order polynomials of the covariates can be used in
+		constructing synthetic controls.
 
 		Arguments
 		---------
 			poly: integer
-				Highest polynomial degree to match up to in constructing synthetic
-				controls.
+				Highest polynomial degree to match up to in constructing
+				synthetic controls.
 
 		Returns
 		-------
 			A Results class instance.
 		"""
-
-		ITT = np.zeros(self.N_t)
 
 		if poly > 1:
-
-			terms = []
-			for power in xrange(2, poly+1):
-				terms.extend(list(itertools.combinations_with_replacement(range(self.k),
-				                                                          power)))
-			num_of_terms = len(terms)
-
-			X_control = self.__polymatrix(self.X[self.controls], terms)
-			
-			for i in xrange(self.N_t):  # can vectorize, but higher space requirement
-				X_treated = self.__polymatrix(self.X[self.treated][i, :].reshape((1, self.k)), terms)
-				w = np.linalg.lstsq(X_control.T, X_treated.ravel())[0]
-				ITT[i] = self.Y[self.treated][i] - np.dot(w, self.Y[self.controls])
-
+			ITT = self.__synthetic(self.__polymatrix(self.X, poly))
 		else:
-			for i in xrange(self.N_t):
-				w = np.linalg.lstsq(self.X[self.controls].T, self.X[self.treated][i, ])[0]
-				ITT[i] = self.Y[self.treated][i] - np.dot(w, self.Y[self.controls])
+			ITT = self.__synthetic(self.X)
 
-		return Results(ITT.mean(), ITT)
-
-
-	def matching_old(self, replace=False, wmatrix=None, correct_bias=False,
-	                 order_by_pscore=False):
-
-		"""
-		Estimates the average treatment effect for the treated (ATT) using matching.
-
-		Can specify whether matching is done with or without replacement.
-
-		By default, the distance metric used for measuring covariate differences
-		is the Euclidean metric. The Mahalanobis metric or other arbitrary weighting
-		matrices can also be used instead.
-
-		Bias correction can optionally be done. Bias resulting from imperfect
-		matches is estimated by
-			(X_t - X_c[matched])' * b,
-		where b is the estimated coefficient from regressiong Y_c[matched] on
-		X_c[matched]. For details, see Imbens and Rubin.
-
-		Matching without replacement is computed via a greedy algorithm, where
-		the best match for a treated unit is chosen without regard to the rest of
-		the sample. By default, treated units are matched in the order that they
-		come in in the data. An option to match in the order of descending estimated
-		propensity score is provided.
-
-		Arguments
-		---------
-			replace: Boolean
-				Match with replacement or not; defaults to without.
-			wmatrix: string, array-like
-				Distance measure to be used; acceptable values are None (Euclidean norm),
-				string 'maha' for Mahalanobis metric, or any arbitrary k-by-k matrix
-			correct_bias: Boolean
-				Correct bias resulting from imperfect matches or not; defaults to
-				no correction.
-			order_by_pscore: Boolean, optional
-				Determines order of match-making when matching without replacement.
-
-		Returns
-		-------
-			A Results class instance.
-		"""
-
-		match_index = np.zeros(self.N_t, dtype=np.int)
-
-		if wmatrix == 'maha':
-			wmatrix = np.linalg.inv(np.cov(self.X, rowvar=False))
-
-		if replace:
-			for i in xrange(self.N_t):
-				dX = self.X[self.controls] - self.X[self.treated][i]  # N_c-by-k matrix
-				match_index[i] = np.argmin(self.__norm(dX, wmatrix))
-		else:
-			unmatched = range(self.N_c)
-			if order_by_pscore:
-				pscore = sm.Logit(self.D, self.X).fit(disp=False).predict()  # estimate by logit
-				order = np.argsort(pscore[self.D==1])[::-1]  # descending pscore order
-			else:
-				order = xrange(self.N_t)
-			for i in order:
-				dX = self.X[self.controls][unmatched] - self.X[self.treated][i]
-				match_index[i] = unmatched.pop(np.argmin(self.__norm(dX, wmatrix)))
-
-		if correct_bias:
-			reg = sm.OLS(self.Y[self.controls][match_index],
-			             sm.add_constant(self.X[self.controls][match_index])).fit()
-			ITT = (self.Y[self.treated] - self.Y[self.controls][match_index] - 
-			       np.dot((self.X[self.treated] - self.X[self.controls][match_index]), reg.params[1:]))
-		else:
-			ITT = self.Y[self.treated] - self.Y[self.controls][match_index]
-
-		return Results(ITT.mean(), ITT)
+		return Results(ITT[self.treated].mean(), ITT.mean(), ITT[self.controls].mean())
 
 
 	def __norm(self, dX, W):
 
 		"""
-		Calculates a vector of norms given weighting matrix W.
+		Calculate vector of norms given weighting matrix W.
 
 		Arguments
 		---------
@@ -173,6 +119,10 @@ class CausalModel(object):
 				Matrix of covariate differences.
 			W: array-like
 				k-by-k weighting matrix or None for Eucliean norm.
+
+		Returns
+		-------
+			Vector of distance measures.
 
 		"""
 
@@ -183,6 +133,27 @@ class CausalModel(object):
 
 
 	def __matchmaking(self, X, X_m, M=1, W=None):
+
+		'''
+		Perform nearest-neigborhood matching using specified weighting
+		matrix in measuring distance. If M > 1, each element of m_indx
+		is itself a list of indices.
+
+		Arguments
+		---------
+			X: array-like
+				Observations to find matches for.
+			X_m: array-like
+				Pool of potential matches.
+			M: integer
+				Number of matches to find per unit.
+			W: array-like
+				k-by-k weighting matrix (of None for Euclidean norm)
+
+		Returns
+		-------
+			List of matched indices.
+		'''
 		
 		n = X.shape[0]
 		m_indx = np.zeros((n, M), dtype=int)
@@ -199,6 +170,21 @@ class CausalModel(object):
 
 	def __bias(self, treated, m_indx):
 
+		'''
+		Estimate bias resulting from imperfect matches using least squares.
+
+		Arguments
+		---------
+			treated: Boolean
+				Indicates subsample to estimate bias for.
+			m_index: array-like
+				Index of matched units.
+
+		Returns
+		-------
+			Vector of estimated biases.
+		'''
+
 		if treated:
 			b = np.linalg.lstsq(np.column_stack((np.ones(m_indx.size), self.X[self.controls][m_indx.ravel()])),
 			                    self.Y[self.controls][m_indx.ravel()])[0][1:]
@@ -211,13 +197,56 @@ class CausalModel(object):
 
 	def matching(self, wmatrix=None, matches=1, correct_bias=False):
 
+		"""
+		Estimate average treatment effects using matching with replacement.
+
+		By default, the distance metric used for measuring covariate
+		differences	is the Euclidean metric. The Mahalanobis metric or other
+		arbitrary weighting	matrices can also be used instead.
+
+		The number of matches per subject can also be specified.
+
+		Bias correction can optionally be done. For treated units, the bias
+		resulting from imperfect matches is estimated by
+			(X_t - X_c[matched]) * b,
+		where b is the estimated coefficient from regressiong Y_c[matched] on
+		X_c[matched]. For control units, the analogous procedure is used.
+		For details, see Imbens and Rubin.
+
+		Matching without replacement is computed via a greedy algorithm, where
+		the best match for a treated unit is chosen without regard to the rest
+		of the sample. By default, treated units are matched in the order that
+		they come in in the data. An option to match in the order of
+		descending estimated propensity score is provided.
+
+		Arguments
+		---------
+			wmatrix: string, array-like
+				Distance measure to be used; acceptable values are None
+				(Euclidean norm, default), string 'maha' for Mahalanobis
+				metric, or any arbitrary k-by-k matrix.
+			matches: integer
+				The number of units to match to a given subject. Defaults
+				to 1.
+			correct_bias: Boolean
+				Correct bias resulting from imperfect matches or not; defaults
+				to no correction.
+			order_by_pscore: Boolean, optional
+				Determines order of match-making when matching without
+				replacement.
+
+		Returns
+		-------
+			A Results class instance.
+		"""
+
 		if wmatrix == 'maha':
 			wmatrix = np.linalg.inv(np.cov(self.X, rowvar=False))
 
 		m_indx_c = self.__matchmaking(self.X[self.controls], self.X[self.treated], matches, wmatrix)
 		m_indx_t = self.__matchmaking(self.X[self.treated], self.X[self.controls], matches, wmatrix)
 
-		ITT = np.zeros(self.N)
+		ITT = np.empty(self.N)
 		ITT[self.controls] = self.Y[self.treated][m_indx_c].mean(1)- self.Y[self.controls]
 		ITT[self.treated] = self.Y[self.treated] - self.Y[self.controls][m_indx_t].mean(1)
 
@@ -225,10 +254,48 @@ class CausalModel(object):
 			ITT[self.controls] -= self.__bias(treated=False, m_indx=m_indx_c)
 			ITT[self.treated] -= self.__bias(treated=True, m_indx=m_indx_t)
 
-		return Results(ITT[self.treated].mean(), ITT)
+		return Results(ITT[self.treated].mean(), ITT.mean(), ITT[self.controls].mean())
 
 
 	def matching_without_replacement(self, wmatrix=None, order_by_pscore=False, correct_bias=False):
+
+		"""
+		Estimate average treatment effects using matching without replacement.
+
+		By default, the distance metric used for measuring covariate
+		differences	is the Euclidean metric. The Mahalanobis metric or other
+		arbitrary weighting	matrices can also be used instead.
+
+		Bias correction can optionally be done. For treated units, the bias
+		resulting from imperfect matches is estimated by
+			(X_t - X_c[matched]) * b,
+		where b is the estimated coefficient from regressiong Y_c[matched] on
+		X_c[matched]. For control units, the analogous procedure is used.
+		For details, see Imbens and Rubin.
+
+		Matching without replacement is computed via a greedy algorithm, where
+		the best match for a treated unit is chosen without regard to the rest
+		of the sample. By default, treated units are matched in the order that
+		they come in in the data. An option to match in the order of
+		descending estimated propensity score is provided.
+
+		Arguments
+		---------
+			wmatrix: string, array-like
+				Distance measure to be used; acceptable values are None
+				(Euclidean norm, default), string 'maha' for Mahalanobis
+				metric, or any arbitrary k-by-k matrix.
+			order_by_pscore: Boolean, optional
+				Determines order of match-making when matching without
+				replacement.
+			correct_bias: Boolean
+				Correct bias resulting from imperfect matches or not; defaults
+				to no correction.
+
+		Returns
+		-------
+			A Results class instance.
+		"""
 
 		m_indx = np.zeros((self.N_t, 1), dtype=np.int)
 
@@ -251,13 +318,13 @@ class CausalModel(object):
 		if correct_bias:
 			ITT -= self.__bias(treated=True, m_indx=m_indx)
 
-		return Results(ITT.mean(), ITT)
+		return Results(ITT.mean())
 
 
 	def ols(self):
 
 		"""
-		Estimates the average treatment effect for the treated (ATT) using least squares.
+		Estimate average treatment effects using least squares.
 
 		Returns
 		-------
@@ -272,18 +339,21 @@ class CausalModel(object):
 		reg = sm.OLS(self.Y, sm.add_constant(Z)).fit()
 		ITT = reg.params[1] + np.dot(dX[self.D==1], reg.params[-self.k:])
 
-		return Results(ITT.mean(), ITT)
+		return Results(ITT.mean(), reg.params[1])
 
 
 class Results(object):
 
-	def __init__(self, ATT, ITT):
-		self.ATT = ATT
-		self.ITT = ITT
+	def __init__(self, ATET, ATE=None, ATUT=None):
+		self.ATET, self.ATE, self.ATUT = ATET, ATE, ATUT
 
 	def summary(self):
 
-		print 'Estimated Average Treatment Effect on the Treated:', self.ATT
+		if self.ATE:
+			print 'Estimated Average Treatment Effect:', self.ATE
+		print 'Estimated Average Treatment Effect on the Treated:', self.ATET
+		if self.ATUT:
+			print 'Estimated Average Treatment Effect on the Untreated:', self.ATUT
 
 
 
