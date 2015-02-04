09ecdcbd09389c4e82e1cf58cf5b7474af8a91a7
Laurence Wong <laurencium@gmail.com>
2015-02-04 09:34:53 -0800

    add function to print tables

--- causalinference/utils/tools.py
+++ causalinference/utils/tools.py
@@ -24,12 +24,7 @@ class Printer(object):
 		self.table_width = 80
 
 
-	def print_sep(self, symbol):
-
-		print symbol * self.table_width
-
-
-	def print_row(self, entries, span, string):
+	def print_row(self, entries, span, etype):
 
 		"""
 		Expected args
@@ -37,16 +32,25 @@ class Printer(object):
 			entries: tuple
 		"""
 
-		l = len(span)
+		k = len(span)
 		cols = sum(span)
-		span = [(self.table_width//cols) * span[i] for i in xrange(l)]
+		span = [(self.table_width//cols) * span[i] for i in xrange(k)]
 		span[0] += self.table_width % cols
-		for i in xrange(l):
-			if string[i]:
-				string[i] = 's'
+		for i in xrange(k):
+			if etype[i] == 'string':
+				etype[i] = 's'
+			elif etype[i] == 'float':
+				etype[i] = '.3f'
 			else:
-				string[i] = '.3f'
+				etype[i] = '.0f'
 
-		line = ['%'+str(span[i])+string[i] for i in xrange(l)]
+		line = ['%'+str(span[i])+etype[i] for i in xrange(k)]
 		print ''.join(line) % entries
+
+
+	def print_table(self, entries, span, etype):
+
+		n = len(entries)
+		for i in xrange(n):
+			self.print_row(entries[i], span[i], etype[i])
 
