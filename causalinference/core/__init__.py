e1910d6c02bf44018489623fc320df4067ce6775
Laurence Wong <laurencium@gmail.com>
2015-06-18 00:17:15 -0700

    reorganize directory structure

--- causalinference/core/__init__.py
+++ causalinference/core/__init__.py
@@ -1,6 +1,4 @@
 from data import Data
 from summary import Summary
-from basic import Basic
-from strata import Stratum, Strata
 from propensity import Propensity, PropensitySelect
 
--- causalinference/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-from test_causal import *
-from test_data import *
-from test_summary import *
-from test_propensity import *
-from test_propensityselect import *
-#from test_trim import *
-#from test_stratify import *
-
